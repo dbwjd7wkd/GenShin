48ad8016b1c2bd49f66d8167234d965b00ce98a0
YuJeong <dbwjd7wkd@naver.com>
2024-12-16 16:52:38 +0900

    캐릭터 스탯 위젯 만드는 중 (스탯컴포넌트와 위젯컴포넌트 간의 느슨한 결합 구조로 만들기)

---
+++ Source/GenShin/UI/GSCharacterWidgetComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "GSCharacterWidgetComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHIN_API UGSCharacterWidgetComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GenShin/UI/GSHpBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/GSUserWidget.h"
+#include "GSHpBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHIN_API UGSHpBarWidget : public UGSUserWidget
+{
+	GENERATED_BODY()
+	
+};
